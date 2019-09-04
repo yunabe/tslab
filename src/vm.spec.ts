52d86bbc7cf51d393533add2666b401f9f4e7a42
Yu Watanabe <yunabe.public@gmail.com>
2019-09-04 22:07:33 +0900

    Check the spec of vm.

---
+++ src/vm.spec.ts
@@ -0,0 +1,27 @@
+import * as vm from "vm";
+
+describe("vmspec", () => {
+  it("basics", () => {
+    const sandbox = {};
+    vm.createContext(sandbox);
+    // Top level var is a global variable.
+    vm.runInContext(`var x = 3;`, sandbox);
+    expect(sandbox).toEqual({ x: 3 });
+    vm.runInContext(
+      `(function() {
+            x = x + 4;
+      })()`,
+      sandbox
+    );
+    expect(sandbox).toEqual({ x: 7 });
+    vm.runInContext(
+      `(function(exports) {
+          x = x * x;
+          let y = x * 2;
+          exports.y = y;
+      })(this)`,
+      sandbox
+    );
+    expect(sandbox).toEqual({ x: 49, y: 98 });
+  });
+});
