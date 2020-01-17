2f8503a7f07081a8487cb2da969dd8e9b8b08643
Yu Watanabe <yunabe.public@gmail.com>
2020-01-18 00:36:52 +0900

    Add a bundle method.

---
+++ src/bundle.spec.ts
@@ -0,0 +1,68 @@
+import { resolve } from "path";
+import { bundle } from "./bundle";
+import * as path from "path";
+
+describe("bundle", () => {
+  const cwd = process.cwd();
+  async function bund(
+    entry: string,
+    files: Map<string, string>
+  ): Promise<string> {
+    const absFiles = new Map<string, string>();
+    for (const [key, val] of files) {
+      absFiles.set(path.join(cwd, key), val);
+    }
+    return await bundle(path.join(cwd, entry), absFiles);
+  }
+
+  it("import", async () => {
+    const out = await bund(
+      "index.js",
+      new Map<string, string>([
+        ["index.js", `const foo = require("./foo");console.log(foo.x);`],
+        ["foo.js", "exports.x = 10;"]
+      ])
+    );
+    const want = [
+      "var x = 10;",
+      "",
+      "var foo = {",
+      "\tx: x",
+      "};",
+      "",
+      "console.log(foo.x);",
+      ""
+    ].join("\n");
+    // console.log(JSON.stringify(out.split("\n")));
+    expect(out).toEqual(want);
+  });
+
+  it("import default", async () => {
+    const out = await bund(
+      "index.js",
+      new Map<string, string>([
+        ["index.js", `const foo = require("./foo");console.log(foo);`],
+        ["foo.js", "module.exports = 'foobarbaz';"]
+      ])
+    );
+    const want = ["var foo = 'foobarbaz';", "", "console.log(foo);", ""].join(
+      "\n"
+    );
+    expect(out).toEqual(want);
+  });
+
+  it("NODE_ENV switch", async () => {
+    // Handle NODE_ENV switch which is used in React.
+    const out = await bund(
+      "index.js",
+      new Map<string, string>([
+        [
+          "index.js",
+          `if (process.env.NODE_ENV === 'production') {console.log('prod');} else {console.log('demo')}`
+        ]
+      ])
+    );
+    const want = "{console.log('prod');}\n";
+    expect(out).toEqual(want);
+  });
+});
---
+++ src/bundle.ts
@@ -0,0 +1,64 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import * as path from "path";
+import * as rollup from "rollup";
+
+function createPlugin(files: Map<string, string>): rollup.Plugin {
+  function getFile(path: string): string {
+    if (files.has(path)) {
+      return path;
+    }
+    path += ".js";
+    if (files.has(path)) {
+      return path;
+    }
+    return null;
+  }
+  return {
+    name: "tslab-virtual",
+    resolveId(source: string, importer: string): string | undefined {
+      if (source.startsWith("\0")) {
+        // Skip this because source is a fake module by other modules.
+        return null;
+      }
+      if (!source.startsWith("./") && !source.startsWith("../")) {
+        return getFile(source);
+      }
+      if (!importer) {
+        return null;
+      }
+      return getFile(path.resolve(path.dirname(importer), source));
+    },
+    load(id: string): string | undefined {
+      if (files.has(id)) {
+        return files.get(id);
+      }
+    }
+  };
+}
+
+// References:
+// https://github.com/rollup/plugins/tree/master/packages/commonjs
+// https://rollupjs.org/guide/en/
+export async function bundle(
+  entry: string,
+  files: Map<string, string>
+): Promise<string> {
+  const bundle = await rollup.rollup({
+    input: entry,
+    plugins: [
+      createPlugin(files),
+      resolve(),
+      commonjs(),
+      // c.f. https://github.com/webpack/webpack/issues/1720
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production")
+      })
+    ]
+  });
+  const { output } = await bundle.generate({
+    format: "esm"
+  });
+  return output[0].code;
+}
